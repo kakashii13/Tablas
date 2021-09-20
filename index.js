dad35d34b1e59ef7b23812d14fb30cba898cdc24
Matias Dominguez <petunilo96@gmail.com>
2021-09-20 15:33:20 -0300

    primera version terminada

---
+++ index.js
@@ -0,0 +1,21 @@
+let table = document.getElementById("table");
+
+calcular();
+
+function calcular() {
+  table.addEventListener("click", () => {
+    let value = parseInt(table.options[table.selectedIndex].text);
+    for (let i = 1; i <= 10; i++) {
+      console.log(`${value} * ${i} = ${value * i}`);
+      let uList = document.getElementById("list");
+      let list = document.createElement("li");
+      list.classList.add("result__list__item");
+      list.innerText = `${value} * ${i} = ${value * i}`;
+
+      uList.appendChild(list);
+
+      let text = document.getElementById("text");
+      text.innerText = "Para mirar otra tabla recargar la pagina.";
+    }
+  });
+}
